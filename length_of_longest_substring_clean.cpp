c1e48b7deef0c8643f45074e6aed34c7ea1b617b
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-23 12:35:35 +0530

    new problem

---
+++ length_of_longest_substring_clean.cpp
@@ -0,0 +1,39 @@
+int lengthOfLongestSubstring(string s) {
+    // Write your code here.
+    // assuming that the string only contains lower case alphabets 
+
+    vector<int> a(26,0);
+    int n = s.size();
+    int st= 0,e=0,maxLen=0;
+    int numDistinct = 0;
+    for(e=0;e<n;e++){
+        int curLen = e-st+1;
+        // if (numDistinct<=2){
+        //     maxLen = max(maxLen,curLen);
+        // }
+
+        if(a[s[e]-'a']!=0){
+          a[s[e] - 'a']++;
+          maxLen = max(curLen,maxLen);   
+        }
+       else  if(a[s[e]-'a']==0 && numDistinct<2){
+            numDistinct++;
+            a[s[e]-'a']++;
+            maxLen = max(curLen,maxLen);
+        }
+        else {
+            a[s[e] - 'a']++;
+            numDistinct++;
+
+            while(numDistinct>2 && st<=e){
+                --( a[s[st]-'a']);
+               if((a[s[st]-'a'])==0){
+                   numDistinct--;
+               }
+                ++st;
+            }
+        }
+    }
+
+    return maxLen;
+}
