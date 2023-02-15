d5ba34be2cf32b6267633ea710676e06ea26bbd2
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-15 22:05:59 +0800

    solution

--- minimum_number_of_swaps.cpp
+++ minimum_number_of_swaps.cpp
@@ -1,40 +1,40 @@
 #include <bits/stdc++.h>
 
-int minSwap (int N, int M, int K, string &S, vector<char> &swaparr)
-{
-    int lookup[26]={0};
-    for(auto i:swaparr){
-        lookup[i-'a']=1;
-    }
+int minSwap(int N, int M, int K, string &S, vector<char> &swaparr) {
+  int lookup[26] = {0};
+  for (auto i : swaparr) {
+    lookup[i - 'a'] = 1;
+  }
 
-
-    int arr[K][26];
-    for (int i =0;i<K;i++){
-        for (int j =0;j<26;j++){
-            arr[i][j]=0;
-        }
+  int arr[K][26];
+  for (int i = 0; i < K; i++) {
+    for (int j = 0; j < 26; j++) {
+      arr[i][j] = 0;
     }
-    
-    for (int i = 0; i <N;i++){
-        int rowindex = (i%K);
-        arr[rowindex][S[i]-'a']++;
-    }
+  }
 
-    // for (int i = 0; i < K; i++) {
-    //   for (int j = 0; j < 26; j++) {
-    //     cout<< arr[i][j] <<" ";
-    //   }
-    //   cout<<endl;
-    // }
+  for (int i = 0; i < N; i++) {
+    int rowindex = (i % K);
+    arr[rowindex][S[i] - 'a']++;
+  }
 
-    int ans = 0;
-    for (int i =0;i<K;i++){
-      int val = *max_element(arr[i], arr[i] + 26);
-      int sum= 0;
-      for (int j =0;j<26;j++){
-        sum+=arr[i][j];
-      }
-      ans += (sum- val);
+  // for (int i = 0; i < K; i++) {
+  //   for (int j = 0; j < 26; j++) {
+  //     cout<< arr[i][j] <<" ";
+  //   }
+  //   cout<<endl;
+  // }
+
+  int ans = 0;
+  for (int i = 0; i < K; i++) {
+    int val = 0;
+    int sum = 0;
+    for (int j = 0; j < 26; j++) {
+      sum += arr[i][j];
+      if (arr[i][j] > val && lookup[j] == 1)
+        val = arr[i][j];
     }
-    return ans;
+    ans += (sum - val);
+  }
+  return ans;
 }
