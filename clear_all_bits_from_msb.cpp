32bfe8135ae6805aacdee41d2b12e79eca910efb
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-23 15:29:08 +0800

    solution

---
+++ clear_all_bits_from_msb.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define ll long long
+#define pb push_back
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+#define fr(i, a, b) for (int i = a; i < b; i++)
+#define loop(x, n) for (int x = 0; x < n; ++x)
+#define mod 1000000007
+#define inf (1LL << 60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal() {}
+
+void solve() {
+  int n, i;
+  cin >> n >> i;
+  int mask = (1<<i)-1;
+  n= n&mask;
+  cout<< n<< "\n";  
+
+}
+
+int main() {
+  fast_io;
+  cout << fixed;
+  cout << setprecision(10);
+  precal();
+  int t = 1;
+  cin >> t;
+  for (int i = 1; i <= t; i++) {
+    // cout<< "Case #"<<i<<": ";
+    solve();
+  }
+
+  return 0;
+}
