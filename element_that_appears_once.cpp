cc7d1d68f80b7f8b9cf3bf0736187293818df3ee
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-23 15:13:12 +0800

    solution

--- element_that_appears_once.cpp
+++ element_that_appears_once.cpp
@@ -4,7 +4,7 @@ int elementThatAppearsOnce(vector<int> arr) {
 	int bits[32]={0};
 	int n = arr.size();
 	int  val = 0;
-	for (int i =0;i<31;i++){
+	for (int i =0;i<32;i++){
 		for (int j =0 ;j<n;j++){
 			if((arr[j]>>i)&1){
 				bits[i]++;
---
+++ unset_ith_bit.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#define ll long long
+#define pb push_back
+#define fast_io                                                                \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(0);                                                                  \
+  cout.tie(0);
+#define fr(i, a, b) for (int i = a; i < b; i++)
+#define loop(x, n) for (int x = 0; x < n; ++x)
+#define mod 1000000007
+#define inf (1LL << 60)
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+void precal() {}
+
+void solve() {
+    int n ;
+    cin>>n;
+    int i ;
+    cin>>i;
+    int mask = (1<<32)-1;
+    mask ^= (1<<i);
+
+    int num = n&mask;
+    cout<<num<<"\n";
+}
+
+int main() {
+  fast_io;
+  cout << fixed;
+  cout << setprecision(10);
+  precal();
+  int t = 1;
+  cin>> t ;
+  for (int i = 1; i <= t; i++) {
+    // cout<< "Case #"<<i<<": ";
+    solve();
+  }
+
+  return 0;
+}
